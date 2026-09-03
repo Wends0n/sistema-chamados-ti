dd0b5f8987c72a8525681f4cbf2ca6d9574abc17
Wendson <wendsonmenezes123@gmail.com>
2026-09-03 11:31:30 -0300

    feat: finaliza DTOs de entrada e saida

--- src/main/java/com/wendson/sistemachamados/dto/ChamadoRequestDTO.java
+++ src/main/java/com/wendson/sistemachamados/dto/ChamadoRequestDTO.java
@@ -2,13 +2,20 @@ package com.wendson.sistemachamados.dto;
 
 import com.wendson.sistemachamados.entity.Estado;
 import com.wendson.sistemachamados.entity.Prioridade;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class ChamadoRequestDTO {
 
+    @NotBlank(message = "O título é obrigatório")
     private String titulo;
+    @NotBlank(message = "A descrição é obrigatória")
     private String descricao;
+    @NotBlank(message = "O solicitante é obrigatório")
     private String solicitante;
+    @NotNull(message = "O estado é obrigatório")
     private Estado estado;
+    @NotNull(message = "A prioridade é obrigatória")
     private Prioridade prioridade;
 
     public String getTitulo() { return titulo; }
@@ -25,4 +32,5 @@ public class ChamadoRequestDTO {
 
     public Prioridade getPrioridade() { return prioridade; }
     public void setPrioridade(Prioridade prioridade) { this.prioridade = prioridade; }
+
 }
--- src/main/java/com/wendson/sistemachamados/dto/ChamadoResponseDTO.java
+++ src/main/java/com/wendson/sistemachamados/dto/ChamadoResponseDTO.java
@@ -5,7 +5,6 @@ import com.wendson.sistemachamados.entity.Prioridade;
 import java.time.LocalDateTime;
 
 public class ChamadoResponseDTO {
-
     private long id;
     private String titulo;
     private String descricao;
@@ -14,18 +13,53 @@ public class ChamadoResponseDTO {
     private Prioridade prioridade;
     private LocalDateTime dataAbertura;
 
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setId(long id){
+        this.id = id;
+    }
+    public long getId(){
+        return id;
+    }
 
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
 
-    public String getSolicitante() { return solicitante; }
-    public void setSolicitante(String solicitante) { this.solicitante = solicitante; }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
 
-    public Estado getEstado() { return estado; }
-    public void setEstado(Estado estado) { this.estado = estado; }
+    public void setSolicitante(String solicitante){
+        this.solicitante = solicitante;
+    }
+    public String getSolicitante(){
+        return solicitante;
+    }
 
-    public Prioridade getPrioridade() { return prioridade; }
-    public void setPrioridade(Prioridade prioridade) { this.prioridade = prioridade;}
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    public Estado getEstado(){
+        return estado;
+    }
+
+    public void setPrioridade(Prioridade prioridade){
+        this.prioridade = prioridade;
+    }
+    public Prioridade getPrioridade(){
+        return prioridade;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura){
+        this.dataAbertura = dataAbertura;
+    }
+    public LocalDateTime getDataAbertura(){
+        return dataAbertura;
+    }
+
 }
