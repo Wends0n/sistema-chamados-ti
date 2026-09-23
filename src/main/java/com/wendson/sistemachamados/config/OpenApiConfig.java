d08529f4d746bfadf0ce04301c5b4977e087c9c1
Wendson <wendsonmenezes123@gmail.com>
2026-09-23 19:26:24 -0300

    docs: configura titulo e descrição da API no OpenAPI

--- src/main/java/com/wendson/sistemachamados/config/OpenApiConfig.java
+++ src/main/java/com/wendson/sistemachamados/config/OpenApiConfig.java
@@ -1,4 +1,22 @@
 package com.wendson.sistemachamados.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI sistemaChamadosOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API do Sistema de Chamados")
+                        .version("1.0.0")
+                        .description(
+                                "API para gerenciamento de usuários, categorias, "
+                                        + "chamados, comentários e históricos."
+                        ));
+    }
 }
